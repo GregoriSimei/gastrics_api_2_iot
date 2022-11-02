8e75a0fabf1d6d4e7030d5db8ec5c93edc475875
GregoriSimei <gregorisimei@gmail.com>
2022-11-02 15:22:25 -0300

    Initialize app

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,19 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
-import { errorHandler } from './infra/http/middlewares/errorHandler';
+import { appConfig } from './config/appConfig';
+import { errorHandler } from './infra/http/midlewares/errorHandler';
+import { logger } from './shared/loggers/logger';
 
+dotenv.config();
+
+const app = express();
+
+app.use(errorHandler);
+
+const port = appConfig.port;
+app.listen(
+    port,
+    (): void => {
+        logger.info(`App runing on port: ${port}`);
+    }
+)
