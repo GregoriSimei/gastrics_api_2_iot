39d0afe0eb760db3b9996d4104472260046f095e
GregoriSimei <gregorisimei@gmail.com>
2022-11-06 15:35:40 -0300

    MongoDb Config file

---
+++ src/config/mongodbConfig.ts
@@ -0,0 +1,6 @@
+export const mongodbConfig = Object.freeze({
+  host: process.env.MONGODB_HOST,
+  port: process.env.MONGODB_PORT,
+  user: process.env.MONGODB_USER,
+  pass: process.env.MONGODB_PASS,
+});
