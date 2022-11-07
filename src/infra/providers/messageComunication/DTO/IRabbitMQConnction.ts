0e26af14625acebd2705d0f39ec96b3c12d2340c
GregoriSimei <gregorisimei@gmail.com>
2022-11-07 00:11:41 -0300

    Adjusted Names

--- src/infra/providers/messageComunication/DTO/IRabbitMQConnction.ts
+++
@@ -1,7 +0,0 @@
-import { Channel, Connection } from 'amqplib';
-
-export interface IRabbitMQConnection {
-  start(): Promise<void>;
-  getConnection(): Promise<Connection>;
-  getChannel(): Promise<Channel>;
-}
