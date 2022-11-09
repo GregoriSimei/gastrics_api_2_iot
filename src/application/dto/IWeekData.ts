eb511952a83647f5921b73527e8d70fb89cd2b75
GregoriSimei <gregorisimei@gmail.com>
2022-11-08 22:39:44 -0300

    Updated weekData model

--- src/application/dto/IWeekData.ts
+++ src/application/dto/IWeekData.ts
@@ -2,6 +2,7 @@ import { IDataPerHour } from './DataPerHour';
 import { weeksLong } from './WeeksLong';
 
 export interface IWeekData {
+  exId: string;
   name: keyof weeksLong;
   weigthAVG: number;
   iteration: number;
