3895dd58cc2d3afbf99b65f5403e10023b77c124
GregoriSimei <gregorisimei@gmail.com>
2022-11-15 15:53:23 -0300

    Removed vars

--- src/application/listeners/cylinderDay.ts
+++ src/application/listeners/cylinderDay.ts
@@ -1,12 +1,6 @@
 import { logger } from '../../shared/loggers/logger';
 import { RabbitMQServer } from '../../infra/providers/messageComunication/app/HabbitMQServer';
 
-const dateManager = new DateManager();
-const weekDataRepository: IWeekDataRepository = new WeekDataRepository();
-const dataGenerator: IDataGenerator = new DataGenerator();
-const calc: ICalc = new Calc();
-const gastricsAppClient: IGastricsAppClient = new GastricsAppClient();
-
 async function cylindersDay() {
   const server = new RabbitMQServer();
   await server.start();
